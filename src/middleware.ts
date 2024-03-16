dd1c1efacde6cffb82f5e8003cd0728fbcff16bd
rania <raniamn03@gmail.com>
2024-03-16 11:31:04 +0700

    fix(middleware): add image to matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -17,5 +17,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|icons|favicon.ico).*)']
+  matcher: ['/((?!api|_next/static|_next/image|icons|favicon.ico|memoar.png).*)']
 }
